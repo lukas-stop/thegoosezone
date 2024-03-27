c0a6260b703238d671a5e3ac12b7a9d8e7e80378
Lukas <46488078+lukas-stop@users.noreply.github.com>
2024-03-27 15:03:31 -0400

    Dark mode and simple font toggle functionality
    - updated functionality for dark mode to properly transition between settings
    - added default values on page load (TODO: change to keep user preferences)
    - added simpleFont mode button to change text to arial when toggled

--- js/settings.js
+++ js/settings.js
@@ -1,12 +1,28 @@
 //PLACEHOLDER - lightmode by default
 //TODO: change to keep user preference
-document.body.classList.add("lightMode");
-var checkbox = document.getElementById("DarkModeBtn"); //get the checkbox to a variable
+
+window.addEventListener("load", (event) => {
+    document.body.classList.add("lightMode");
+    settingsMenu.classList.add("settingsLightMode");
+    document.getElementById("playIcon").style.filter = "invert(0)";
+    document.getElementById("bgImg").style.filter = "invert(0)";
+
+    document.getElementById("DarkModeBtn").checked = false;
+    document.getElementById("SimpleFontBtn").checked = false;
+});
+
+
+
+
+// ----------------------------------------
+//  DARK/LIGHT MODE
+// ----------------------------------------
+var darkModeToggle = document.getElementById("DarkModeBtn"); //get the checkbox to a variable
 
 //if the checkbox state is changed, run a function
-checkbox.addEventListener("change", function () {
+darkModeToggle.addEventListener("change", function () {
     //check if the checkbox is checked or not
-    if (checkbox.checked) {
+    if (darkModeToggle.checked) {
         console.log("Dark mode on");
         darkModeActive();
     } else {
@@ -17,14 +33,14 @@ checkbox.addEventListener("change", function () {
 
 var settingsMenu = document.getElementById("settingsMenuJS");
 
-
 function darkModeActive() {
     settingsMenu.classList.remove("settingsLightMode");
     settingsMenu.classList.add("settingsDarkMode");
     document.body.classList.remove("lightMode");
     document.body.classList.add("darkMode");
 
-    //TODO: invert image colors 
+    document.getElementById("playIcon").style.filter = "invert(1)";
+    document.getElementById("bgImg").style.filter = "invert(1)";
 }
 
 function darkModeInActive() {
@@ -32,4 +48,25 @@ function darkModeInActive() {
     settingsMenu.classList.add("settingsLightMode");
     document.body.classList.remove("darkMode");
     document.body.classList.add("lightMode");
+
+    document.getElementById("playIcon").style.filter = "invert(0)";
+    document.getElementById("bgImg").style.filter = "invert(0)";
 }
+
+// ----------------------------------------
+//  SIMPLE FONT MODE
+// ----------------------------------------
+
+var simpleFontToggle = document.getElementById("SimpleFontBtn"); //get the checkbox to a variable
+
+//if the checkbox state is changed, run a function
+simpleFontToggle.addEventListener("change", function () {
+    //check if the checkbox is checked or not
+    if (simpleFontToggle.checked) {
+        console.log("Simple Font Mode on");
+        document.getElementById("homeNavMenu").style.fontFamily = "Arial";
+    } else {
+        console.log("Simple Font Mode off");
+        document.getElementById("homeNavMenu").style.fontFamily = "'Gerak', 'Comic Sans', sans-serif";
+    }
+});
