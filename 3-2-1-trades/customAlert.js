16421d71ba2b011276a53916863985a1a9689555
Lukas <46488078+lukas-stop@users.noreply.github.com>
2026-03-20 18:05:13 -0400

    3-2-1 tradeguide intitial progress save
    basic lookup functionality from drop down menu and starting to add code for equivalent values based on 2:1 rule

---
+++ 3-2-1-trades/customAlert.js
@@ -0,0 +1,15 @@
+msg = `<div style="text-align: center;">
+        <h1 style="font-size: 2em">STOP!</h1>
+        This tool is a work in progress and is not currently<br>
+        intended to be used for high value trading!<br>
+        This tool does not currently check for rerelease pets<br>
+        or store pets.  These are future features I wish to <br>
+        add later.<br><br>
+
+        For additional trading advice and resources, please <br>
+        check out the <a href="https://www.chickensmoothie.com/Forum/viewforum.php?f=135">trade guide forum</a>.
+    </div>`
+
+function customAlert(msg) {
+    alert(msg)
+}
---
+++ 3-2-1-trades/trade-const.js
@@ -0,0 +1,50 @@
+const rarities = [
+    {
+        tag: "OMG so common",
+        abbreviation: "omgsc"
+    },
+    {
+        tag: "Extremely common",
+        abbreviation: "ec"
+    },
+    {
+        tag: "Very common",
+        abbreviation: "vc"
+    },
+    {
+        tag: "Common",
+        abbreviation: "c"
+    },
+    {
+        tag: "Uncommon",
+        abbreviation: "uc"
+    },
+    {
+        tag: "Very uncommon",
+        abbreviation: "vc"
+    },
+    {
+        tag: "Extremely uncommon",
+        abbreviation: "euc"
+    },
+    {
+        tag: "Rare",
+        abbreviation: "r"
+    },
+    {
+        tag: "Very rare",
+        abbreviation: "vr"
+    },   
+    {
+        tag: "Extremely rare",
+        abbreviation: "er"
+    },
+    
+    {
+        tag: "OMG so rare",
+        abbreviation: "omgsr"
+    }
+]
+
+const years = [ 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026 ]
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
